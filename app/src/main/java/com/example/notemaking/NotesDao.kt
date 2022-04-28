480a51f9c417464f4c07f8f0111b9c3c9a57897a
AyushSantri <ayushsantri222@gmail.com>
2022-04-28 17:16:40 +0530

    wrapped DAO get content from LiveData<>

--- app/src/main/java/com/example/notemaking/NotesDao.kt
+++ app/src/main/java/com/example/notemaking/NotesDao.kt
@@ -1,5 +1,6 @@
 package com.example.notemaking
 
+import androidx.lifecycle.LiveData
 import androidx.room.*
 
 @Dao
@@ -13,11 +14,19 @@ interface NotesDao {
     suspend fun insert(notes: Notes)
 
     @Query("SELECT * from notes_table ORDER by id ASC")
-    fun getAllNotes() : List<Notes>
+    fun getAllNotes() : LiveData<List<Notes>>
 }
 
 
-
+/*---------------------------SUSPEND KEYWORD-----------------------------------------------*/
 /*suspend means that particular function will be called from background thread or suspend function
 * the main reason to do this was to not include it in our main thread
 * as insert and delete operations are heavy and if we include it in our main thread, it will make our app laggy*/
+
+
+/*------------------------------LiveData<>---------------------------------------------------*/
+
+/*LiveData follows the observer pattern.
+LiveData notifies Observer objects when underlying data changes.
+You can consolidate your code to update the UI in these Observer objects.
+That way, you don't need to update the UI every time the app data changes because the observer does it for you.*/
