cce5dddc1a6de0baa1068d7a86876aafff0b14fd
AyushSantri <ayushsantri222@gmail.com>
2022-04-28 22:01:52 +0530

    created a singleton abstract class for Room Database

---
+++ app/src/main/java/com/example/notemaking/NoteDatabase.kt
@@ -0,0 +1,33 @@
+package com.example.notemaking
+
+import android.content.Context
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+abstract class NoteDatabase  : RoomDatabase(){
+
+    abstract fun getNotes() : NotesDao
+
+
+    companion object {
+        // Singleton prevents multiple instances of database opening at the
+        // same time.
+        @Volatile
+        private var INSTANCE: NoteDatabase? = null
+
+        fun getDatabase(context: Context): NoteDatabase {
+            // if the INSTANCE is not null, then return it,
+            // if it is, then create the database
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    NoteDatabase::class.java,
+                    "notes_table"
+                ).build()
+                INSTANCE = instance
+                // return instance
+                instance
+            }
+        }
+    }
+}
