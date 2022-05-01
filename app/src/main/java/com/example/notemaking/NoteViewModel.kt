5dc4d59cc727eaf66598768f1e28d6cd343d6490
AyushSantri <ayushsantri222@gmail.com>
2022-05-01 16:40:59 +0530

    implemented view model

---
+++ app/src/main/java/com/example/notemaking/NoteViewModel.kt
@@ -0,0 +1,16 @@
+package com.example.notemaking
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+
+class NoteViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val allNotes : LiveData<List<Notes>>
+
+    init {
+        val dao = NoteDatabase.getDatabase(application).getNotes()
+        val repository = NoteRepository(dao)
+        allNotes = repository.allNotes
+    }
+}
